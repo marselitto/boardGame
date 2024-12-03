7f29bb24be0e1adc08b26c214c3caefc69d7b5b8
szybek <jakub.jaworowski@cyfroweszkoly.pl>
2024-12-03 14:57:55 +0100

    first

---
+++ BoardGameSim/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Player
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
